cacefe2cd2b023610960f062de90cc78dcc7dc4c
Thiago Colares <thiago@apimenti.com.br>
2014-09-07 00:36:14 -0300

    passo-05

--- js/controllers.js
+++ js/controllers.js
@@ -4,35 +4,8 @@
 
 var protocolosApp = angular.module('protocolosApp', []);
 
-protocolosApp.controller('EmpresaCtrl', function($scope) {
-
-	$scope.empresas = [
-		{
-			'nome': 'Vivo',
-			'descricao': 'Celular, Fixo, Internet, TV',
-			'telefone': '555555'
-		},
-		{
-			'nome': 'NET Serviços',
-			'descricao': 'TV, Banda Larga e Telefone',
-			'telefone': '6666666'
-		},
-		{
-			'nome': 'Oi',
-			'descricao': 'Móvel, Fixo, Internet, TV',
-			'telefone': '7777777'
-		},
-		{
-			'nome': 'Tim',
-			'descricao': 'Celular',
-			'telefone': '88888'
-		},
-		{
-			'nome': 'Sky',
-			'descricao': 'TV por assinatura',
-			'telefone': '9999999'
-		},
-	];
-
-	//$scope.empresa = null;
+protocolosApp.controller('EmpresaCtrl', function($scope, $http) {
+	$http.get('empresas.json').success( function(data) {
+		$scope.empresas = data;
+	});
 });
