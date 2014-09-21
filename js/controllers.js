a28900efaccf007c3ada757002d24f9b0f180a0a
Thiago Colares <thiago@apimenti.com.br>
2014-09-21 20:28:15 -0300

    passo-11

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,12 @@
 var protocoloControllers = angular.module('protocoloControllers', []);
 
 protocoloControllers.controller('GerenciarReclamacoesCtrl', function($scope, $http) {
-	$http.get('empresas.json').success( function(data) {
+	$http.get('json/empresas.json').success( function(data) {
 		$scope.empresas = data;
+	});
+
+	$http.get('json/reclamacoes.json').success( function(data) {
+		$scope.reclamacoes = data;
 	});
 
 	$scope.adicionarReclamacao = function() {
@@ -14,6 +18,12 @@ protocoloControllers.controller('GerenciarReclamacoesCtrl', function($scope, $ht
 		$scope.reclamacao = {};
 		$scope.reclamacaoForm.$setPristine();
 	}
+});
 
-	$scope.reclamacoes = [];
+protocoloControllers.controller('ReclamacaoCtrl', function($scope, $http, $routeParams) {
+	$http.get('json/reclamacoes.json').success( function(data) {
+		$scope.reclamacoes = data;
+		console.log($scope.reclamacoes);
+		$scope.reclamacao = $scope.reclamacoes[$routeParams.reclamacaoId];
+	});
 });
