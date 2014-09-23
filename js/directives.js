f7d197eff7876805f165a40295ff237c6c48967c
Thiago Colares <thiago@apimenti.com.br>
2014-09-22 23:40:47 -0300

    passo-14

--- js/directives.js
+++ js/directives.js
@@ -4,6 +4,19 @@ var protocoloDirectives = angular.module('protocoloDirectives', []);
 
 protocoloDirectives.directive('proEmpresa', function() {
 	return {
+		restrict: 'E',
+		scope: {
+			empresaDados: '=empresa'
+		},
 		templateUrl: "partials/empresa.html"
 	}
-})
+});
+
+protocoloDirectives.directive('proObrigatorio', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			element.html(element.text() + ' <span style="color: red;">*</span>');
+		}
+	}
+});
